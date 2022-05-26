8da4a4b1ea8d43b423a53848b82dbdfad6d3e7a7
bhs0906 <hg5112@naver.com>
2022-05-26 22:37:37 +0900

    220526

---
+++ src/homework07/FloatToInteger.java
@@ -0,0 +1,14 @@
+package homework07;
+
+//int형 변화에 실수값을 대입
+
+class FloatToInteger {
+    public static void main(String[] args) {
+        int a;
+		
+        a=(int)10.0;
+        //a=10.0;
+		
+        System.out.println("a = "+a);
+    }
+}
---
+++ src/homework07/PrintABC.java
@@ -0,0 +1,9 @@
+package homework07;
+
+class PrintABC {
+public static void main(String[] args) {
+		System.out.println("\"ABC\\n\"");
+
+	}
+
+}
