4eb60ba714659fd2805c0d9790dc89c2298ff0db
bhs0906 <hg5112@naver.com>
2022-06-09 19:59:33 +0900

    220609

---
+++ src/homework19/Plane2D.java
@@ -0,0 +1,6 @@
+package homework19;
+
+//--- 2차원 인터페이스 ---//
+public interface Plane2D {
+	int getArea();   // 면적을 구한다
+}
---
+++ src/homework19/Players.java
@@ -0,0 +1,7 @@
+package homework19;
+
+//플레이어 인터페이스 Players
+public interface Players {
+	void play();   // 재생
+	void stop();   // 정지
+}
